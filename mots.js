a51aa03ef6f353a9e74b699f8306a7769458a435
nath54 <cerisara.nathan@gmail.com>
2019-12-15 14:08:49 +0100

    a

--- mots.js
+++ mots.js
@@ -2,8 +2,34 @@ const mots=[
     ["ECRAN","Dispositif d'affichage électronique d'images ou de données.","Hardware"],
     ["CLAVIER","Périphérique servant à récupérer les informations de l'utilisateur et a les transférer à l'ordinateur","Hardware"],
     ["SOURIS","Périphérique servant à récupérer les informations de l'utilisateur et a les transférer à l'ordinateur","Hardware"],
-    ["CARTE-GRAPHIQUE","Composant d'ordinateur chargé de l'affichage sur l'ecran","Hardware"]
-];
+    ["CARTE-GRAPHIQUE","Composant d'ordinateur chargé de l'affichage sur l'ecran","Hardware"],
+    ["ANTIBIOTIQUE","Substance d'origine naturelle ou synthétique, ayant la capacité d’arrêter la multiplication des bactéries.","Santé"],
+    ["ANTICORPS","Molécule produite par le système immunitaire pour détecter et neutraliser les agents pathogènes, en particulier les virus.","Santé"],
+    ["ANTIGENES","Substance étrangère à un organisme et provoquant chez celui-ci la constitution d'un anticorps (réaction immunitaire).","Santé"],
+    ["ANTIBIOGRAMME","Analyse permettant de déterminer la sensibilité d’une bactérie aux antibiotiques et de la classer comme sensible, résistante et intermédaire.","Santé"],
+    ["BACTERIE","Micro-organisme unicellulaire dépourvu de chlorophylle et visible uniquement au microscope. Les bactéries sont des éléments qui n'appartiennent ni au règne végétal, ni au règne animal.","Santé"],
+    ["BIOCHIMIE","Science qui étudie la corrélation entre la structure des molécules naturelles et les conséquences sur leur activité.","Santé"],
+    ["CAMPYLOBACTER","Genre de bactéries Gram négatives pouvant provoquer des intoxications alimentaires.","Santé"],
+    ["CANDIDA-ALBICANS","Espèce de levure la plus importante et la plus connue du genre Candida. Elle provoque des infections (candidose), essentiellement au niveau des muqueuses digestives et gynécologiques.","Santé"],
+    ["CARBAPENEMASES","Enzymes de type b-lactamases hydrolysant les carbapénèmes, une classe d’antibiotiques à spectre d’activité antimicrobienne large, principalement utilisés dans le traitement des infections à bactéries multirésistantes.","Santé"],
+    ["Chromogène","Molécule ayant la capacité d’émettre une couleur dans certaines conditions. Incorporée dans un milieu de culture, elle révèle la présence d’une enzyme spécifique d’une bactérie donnée, permettant ainsi l’identification de cette dernière","Santé"],
+    ["ENZYME","Macromolécule protéique qui accélère une réaction biochimique.","Santé"],
+    ["EMBOLIE-PULMONAIRE","obstruction du système artériel irriguant le poumon par un caillot circulant dans le sang.","Santé"],
+    ["ENDOTOXINE","substance située dans la membrane externe de certaines bactéries à Gram négatif, pouvant provoquer de fortes fièvres.","Santé"],
+    ["CACHE","Endotoxine : substance située dans la membrane externe de certaines bactéries à Gram négatif, pouvant provoquer de fortes fièvres.","Informatique"],
+    ["FIRMWARE","Partie logicielle du hardware permettant à celui-ci de fonctionner. Contrairement au BIOS, le firmware peut prendre en charge des fonctionnalités plus évoluées (contenir un système d'exploitation, par exemple).","Informatique"],
+    ["ANTISPAM","Procédé permettant d'éviter d'être victime de spams.","Informatique"],
+    ["APACHE","Serveur web Open Source. Le plus populaire et le plus respectueux de la norme d'affichage (HTML) des sites Internet. Il est utilisé par la majorité des hébergeurs, en environnement Linux, pour sa souplesse et sa sécurité.","Informatique"],
+    ["BANDE-PASSANTE","Débit la largeur d'une ligne de communication","Hardware"],
+    ["BINAIRE"," Langage traité par les ordinateurs ","Informatique"],
+    ["FIREWALL","Machine ou programme placé entre l'Internet et un réseau afin de renforcer la sécurité de ce dernier","Informatique"],
+    ["LINUX"," Système d'exploitation appartenant au domaine public créé par Linus Thorvalds.","Informatique"],
+    ["MODEM","Matériel permettant aux ordinateurs de communiquer par l'intermédiaire du réseau téléphonique.","Informatique"],
+    ["PASSWORD","Il permet de protéger le login qui lui est associé.","Informatique"],
+    ["PIXEL"," Le plus petit élément composant une image numérique ou l'affichage de votre écran.","Hardware"],
+    ["VIRUS"," Programme dont l'objectif est de se disséminer et d'infecter le plus de machines possible.","Informatique"]
+];    
+
 //    MOT                  Indice                                               Thème
 
 const imgs=["imgs/img1.png","imgs/img2.png","imgs/img3.png","imgs/img4.png","imgs/img5.png","imgs/img6.png","imgs/img7.png","imgs/img8.png","imgs/img9.png","imgs/img10.png","imgs/img11.png","imgs/img12.png"];
